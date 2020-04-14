eb074948dfc78e4e21c29457174b775940203676
HelloMan <37075549+rnysy@users.noreply.github.com>
2020-04-14 11:08:07 +0900

    Webclass Homework01
    hi

---
+++ src/test/java/kr/hs/dgsw/webclass1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.webclass1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
